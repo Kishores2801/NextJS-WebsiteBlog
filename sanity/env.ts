049c8c7b1a76d91dcf1875c472db17ccf146e494
Kishore <87652286+Kishores2801@users.noreply.github.com>
2026-05-05 12:28:20 +0530

    Updated website

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,9 +1,11 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-10-31'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+export const dataset =
+  process.env.SANITY_STUDIO_DATASET || 'production'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'upj9dth5'
+export const projectId =
+  process.env.SANITY_STUDIO_PROJECT_ID || 'upj9dth5'
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
